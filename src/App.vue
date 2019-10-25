dbb49b460d75060d9f95932b878fa083ccbae918
Sam~Lubbers <lubbers.sc@gmail.com>
2019-10-25 16:42:18 +0200

    vuetify setup

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
-  <div id="app">
+  <v-app>
     <router-view />
-  </div>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-</style>
+<script>
+export default {
+  name: "App"
+};
+</script>
