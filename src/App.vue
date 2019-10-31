5fc41612b415750a0d3e8598554b47489e3b69f0
Sam~Lubbers <lubbers.sc@gmail.com>
2019-10-31 17:58:52 +0100

    app bar title

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,7 @@
 <template>
   <v-app>
-    <the-navigation-drawer
-      v-model="drawer"
-    />
-    <v-app-bar
-      app
-    >
-      <v-app-bar-nav-icon
-        v-if="$vuetify.breakpoint.smAndDown"
-        @click.stop="toggleDrawer"
-      />
-      <v-toolbar-title>Set title here</v-toolbar-title>
-      <v-spacer />
-      <v-btn
-        icon
-        @click="toggleDarkMode"
-      >
-        <v-icon>mdi-invert-colors</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <the-navigation-drawer v-model="drawer" />
+    <the-app-bar @toggleDrawer="drawer = !drawer" />
     <v-content>
       <!-- Components do not automatically re-render with dynamic routes
       https://router.vuejs.org/guide/essentials/dynamic-matching.html#reacting-to-params-changes
@@ -31,23 +14,17 @@
 
 <script>
 import TheNavigationDrawer from '@/components/TheNavigationDrawer.vue'
+import TheAppBar from '@/components/TheAppBar.vue'
 
 export default {
   name: "App",
   components: {
-    TheNavigationDrawer
+    TheNavigationDrawer,
+    TheAppBar
   },
   data: () => ({
     drawer: null, // vuetify determines initial state based on screen size
-  }),
-  methods: {
-    toggleDrawer: function () {
-      this.drawer = !this.drawer
-    },
-    toggleDarkMode: function () {
-      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
-    }
-  }
+  })
 };
 </script>
 
