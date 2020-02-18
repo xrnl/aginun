6eb3e1b8afcd63659f9c755003f7c268c135e4db
Sam~Lubbers <lubbers.sc@gmail.com>
2020-02-18 11:47:54 +0100

    new role button and dialog

---
+++ src/components/NewItemButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-btn color="primary" @click.stop="$emit('click')">
+    <v-icon>
+      mdi-plus
+    </v-icon>
+    New role
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "NewItemButton",
+};
+</script>
---
+++ src/components/NewItemDialog.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-dialog :value="value" width="600px" @input="$emit('input', false)">
+    <v-card>
+      <v-card-actions class="d-flex justify-end">
+        <v-btn color="primary" text @click="$emit('input', false)">
+          Cancel
+        </v-btn>
+
+        <v-btn color="primary" @click="$emit('input', false)">
+          Submit
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "NewItemDialog",
+  props: {
+    value: {
+      required: true,
+      type: Boolean,
+    },
+  },
+  data: () => ({}),
+};
+</script>
+
+<style lang="scss" scoped></style>
