351d049e76ca1958ea31efe51ce5375b8bad5b4e
abudaan <daniel@abumarkub.net>
2020-03-02 14:06:47 +0100

    reverted task part to veux

--- src/components/tasks/TaskCard.vue
+++ src/components/tasks/TaskCard.vue
@@ -4,13 +4,13 @@
       {{ task.title }}
     </template>
     <template #subtitle>
-      {{ task.local_group.name }}, {{ task.working_group.name }}
+      {{ task.localGroup.text }}, {{ task.workingGroup.text }}
     </template>
     <template #meta>
       <span class="d-flex flex-column justify-center">
         <span class="title flex-grow-0" style="line-height: 1rem">
-          {{ task.time_commitment_min }} -
-          {{ task.time_commitment_max }}
+          {{ task.timeCommitment.min }} -
+          {{ task.timeCommitment.max }}
         </span>
         <span class="overline text-uppercase">hours</span>
       </span>
