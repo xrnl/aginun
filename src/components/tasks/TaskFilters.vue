0b879260db4ff3f2a2d2208eb298fcf25042f606
abudaan <daniel@abumarkub.net>
2020-03-02 01:12:14 +0100

    cleaned up task part

--- src/components/tasks/TaskFilters.vue
+++ src/components/tasks/TaskFilters.vue
@@ -3,8 +3,8 @@
   <div>
     <div>
       <v-text-field
-        :value="searchString"
-        label="Facilitator, Writer, Photographer..."
+        :value="selectedFilters.text"
+        label="Poster design, Outreach, Photographer..."
         class="mt-3"
         @input="value => onSetFilter(value, 'text')"
       />
@@ -15,11 +15,13 @@
       </template>
       <flex-wrapper direction="column">
         <autocomplete-custom
+          :value="selectedFilters.localGroup"
           :items="localGroups"
           label="Local Group"
           @change="id => onSetFilter(id, 'localGroup')"
         />
         <autocomplete-custom
+          :value="selectedFilters.workingGroup"
           :items="workingGroups"
           label="Working Group"
           @change="id => onSetFilter(id, 'workingGroup')"
@@ -31,13 +33,12 @@
         Time commitment
       </template>
       <v-range-slider
-        :value="selectedTimeCommitment"
-        :min="timeCommitmentRange[0]"
-        :max="timeCommitmentRange[1]"
+        v-model="timeRange"
+        :min="timeCommitment.min"
+        :max="timeCommitment.max"
         class="mt-12"
         thumb-label="always"
         label="Time Commitment"
-        @change="id => onSetFilter(id, 'timeCommitment')"
       />
     </filter-section>
   </div>
@@ -46,21 +47,8 @@
 <script>
 import FlexWrapper from "@/components/layout/FlexWrapper.vue";
 import AutocompleteCustom from "@/components/AutocompleteCustom";
+import { mapState } from "vuex";
 import FilterDrawerSection from "../layout/FilterDrawerSection";
-import {
-  TaskAmount,
-  NavbarHeight,
-  LocalGroups,
-  WorkingGroups,
-  SearchString,
-  SelectedTimeCommitment,
-  BoundsTimeCommitmentRange,
-  UpdateTimeCommitmentRange,
-  UpdateLocalGroups,
-  UpdateWorkingGroups,
-  UpdateSearchString,
-  ClearFilter,
-} from "@/apollo/gql/task.gql";
 
 export default {
   name: "TaskFilters",
@@ -69,85 +57,21 @@ export default {
     AutocompleteCustom,
     FlexWrapper,
   },
-  data: () => ({
-    timeCommitmentRange: [],
-    selectedTimeCommitment: [],
-  }),
-  // beforeCreate: () => {
-  //   console.log();
-  // },
-  apollo: {
-    navbarHeight: {
-      query: NavbarHeight,
-      update: data => data.navbarHeight,
+  props: {
+    selectedFilters: {
+      type: Object,
+      required: true,
     },
-    searchString: {
-      query: SearchString,
-      update: data => data.searchString,
-    },
-    taskAmount: {
-      query: TaskAmount,
-      update: data => data.taskAmount,
-    },
-    localGroups: {
-      query: LocalGroups,
-      update: data =>
-        data.local_group.map(({ id, name }) => ({ id, text: name })),
-    },
-    workingGroups: {
-      query: WorkingGroups,
-      update: data =>
-        data.working_group.map(({ id, name }) => ({ id, text: name })),
-    },
-    selectedTimeCommitment: {
-      query: SelectedTimeCommitment,
-      update: data => data.selectedTimeCommitment,
-    },
-    timeCommitmentRange: {
-      query: BoundsTimeCommitmentRange,
-      update: function(data) {
-        const range = [
-          data.role_aggregate.aggregate.min.time_commitment_min,
-          data.role_aggregate.aggregate.max.time_commitment_max,
-        ];
-        this.$apollo.mutate({
-          mutation: UpdateTimeCommitmentRange,
-          variables: { range },
-        });
-        return range;
-      },
-    },
+    roleAmount: { type: Number, default: 0 },
+    onSetFilter: { required: true, type: Function },
   },
-  methods: {
-    clearFilter: function() {
-      this.$apollo.mutate({
-        mutation: ClearFilter,
-      });
-    },
-    onSetFilter: function(value, key) {
-      // console.log(key, value);
-      if (key === "localGroup") {
-        this.$apollo.mutate({
-          mutation: UpdateLocalGroups,
-          variables: { names: value },
-        });
-      } else if (key === "workingGroup") {
-        this.$apollo.mutate({
-          mutation: UpdateWorkingGroups,
-          variables: { names: value },
-        });
-      } else if (key === "timeCommitment") {
-        this.$apollo.mutate({
-          mutation: UpdateTimeCommitmentRange,
-          variables: { range: value },
-        });
-      } else if (key === "text") {
-        this.$apollo.mutate({
-          mutation: UpdateSearchString,
-          variables: { search: value },
-        });
-      }
-    },
+  data: () => ({
+    timeRange: [1, 30],
+  }),
+  computed: {
+    ...mapState("roles", ["timeCommitment"]),
+    ...mapState("localGroups", ["localGroups"]),
+    ...mapState("workingGroups", ["workingGroups"]),
   },
 };
 </script>
