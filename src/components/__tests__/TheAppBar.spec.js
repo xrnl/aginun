ce027b5d1a1b8292b61f2c6483819d1388cb9fa6
Sam~Lubbers <lubbers.sc@gmail.com>
2020-04-12 08:45:12 +0200

    refactored app bar test

--- src/components/__tests__/TheAppBar.spec.js
+++ src/components/__tests__/TheAppBar.spec.js
@@ -4,47 +4,31 @@ import TheAppBar from "@/components/TheAppBar.vue";
 import Vuex from "vuex";
 import Vuetify from "vuetify";
 
-const localVue = createLocalVue();
-localVue.use(Vuex);
-
 describe("TheAppBar", () => {
-  let vuetify;
   let store;
+  let vuetify;
+  const localVue = createLocalVue();
+  localVue.use(Vuex);
+
+  beforeAll(() => {
+    store = new Vuex.Store({ state: { styles: { navbarHeight: "64px" } } });
+    vuetify = new Vuetify({ theme: { dark: false } });
+  });
 
   const mountFunction = options => {
     return mount(TheAppBar, {
-      store,
       localVue,
+      store,
       vuetify,
       ...options,
     });
   };
 
-  beforeEach(() => {
-    store = new Vuex.Store({
-      state: {
-        styles: {
-          navbarHeight: "64px",
-        },
-      },
-    });
-    vuetify = new Vuetify({
-      theme: {
-        dark: false,
-      },
-    });
-  });
-
-  it("application starts in light mode", () => {
-    const wrapper = mountFunction();
-    expect(wrapper.vm.$vuetify.theme.dark).toBeFalsy();
-  });
-
   it("toggles between light and dark mode", async () => {
     const wrapper = mountFunction();
-    const startState = wrapper.vm.$vuetify.theme.dark;
-    wrapper.find(".v-btn").trigger("click");
+    const isDark = wrapper.vm.$vuetify.theme.dark;
+    wrapper.find("button").trigger("click");
     await localVue.nextTick();
-    expect(wrapper.vm.$vuetify.theme.dark).toEqual(!startState);
+    expect(wrapper.vm.$vuetify.theme.dark).not.toBe(isDark);
   });
 });
