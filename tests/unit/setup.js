97a122abbbadb9a34739d65cc6500cbc2bb2b746
Sam~Lubbers <lubbers.sc@gmail.com>
2020-04-12 15:37:08 +0200

    annotation in global testing config

--- tests/unit/setup.js
+++ tests/unit/setup.js
@@ -1,3 +1,8 @@
+/*
+At the moment this code is loaded before all tests,
+but it should only be loaded when testing Vue components,
+and not the Vuex store or GraphQL queries.
+*/
 import Vue from "vue";
 import Vuetify from "vuetify";
 
