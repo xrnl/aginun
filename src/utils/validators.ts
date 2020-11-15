d6ffae5a80c6c22568f1865662c3f5d22f84fd89
Richard Boomsma <richardboomsma@gmail.com>
2020-11-15 10:36:30 +0100

    Added TS

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,7 +1,9 @@
 import has from "lodash/has";
 
-export const makeObjectValidator = objectExpectedStructure => {
-  const isObjectValid = objectToValidate => {
+export const makeObjectValidator = (objectExpectedStructure: {
+  [x: string]: any;
+}) => {
+  const isObjectValid = (objectToValidate: { [x: string]: any; }) => {
     return Object.keys(objectExpectedStructure).every(key => {
       if (!has(objectToValidate, key)) return false;
 
@@ -13,14 +15,16 @@ export const makeObjectValidator = objectExpectedStructure => {
   return isObjectValid;
 };
 
-export const isArrayValid = (arrayToValidate, isItemValid) =>
-  arrayToValidate.every(item => isItemValid(item));
+export const isArrayValid = (
+  arrayToValidate: any[],
+  isItemValid: (arg0: any) => any
+) => arrayToValidate.every((item: any) => isItemValid(item));
 
-export const iconValidator = iconName => {
+export const iconValidator = (iconName: string) => {
   return iconName.startsWith("mdi-");
 };
 
-export const hrefValidator = href => {
+export const hrefValidator = (href: string) => {
   const validHrefPrefixes = ["http://", "https://", "mailto:", "tel:"];
   return validHrefPrefixes.some(hrefPrefix => href.startsWith(hrefPrefix));
 };
