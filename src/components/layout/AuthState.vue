a56fc834d21ee85aced918969a2406d25a20282a
Bas van Loon <basvanloon@live.nl>
2021-02-11 11:23:18 +0100

    more feedback

--- src/components/layout/AuthState.vue
+++ src/components/layout/AuthState.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog v-model="dialog" width="365px" height="285px">
+  <v-dialog v-model="dialog" width="380px" height="285px">
     <template v-slot:activator="{}">
       <v-btn v-if="!loggedIn" @click="dialog = true">{{ $t("Login") }}</v-btn>
       <v-btn v-else text @click="logout">
@@ -7,65 +7,115 @@
       </v-btn>
     </template>
     <v-card id="login-modal">
-      <div class="container">
-        <button id="close-button" @click.prevent="dialog = false"></button>
-        <h2>{{ $t("Member login") }}</h2>
-        <p>
-          {{
-            $t(
-              "Log in to post new vacancies or edit existing ones. Don't have an account yet?"
-            )
-          }}
-        </p>
-        <a href="`mailto:${contactEmail}`">{{ $t("Contact us") }}</a>
-      </div>
-      <validation-observer v-slot="{ invalid }">
-        <form @submit.prevent="login">
-          <div class="container">
-            <validation-provider
-              name="Username"
-              rules="required"
-              v-slot="{ errors }"
-            >
-              <v-text-field
-                v-model="username"
-                :placeholder="$t('Username')"
-                label="Username"
-                :error-messages="errors"
-                autofocus
-                class="mt-3"
-              />
-            </validation-provider>
-            <validation-provider
-              name="Password"
-              rules="required"
-              v-slot="{ errors }"
-            >
-              <v-text-field
-                v-model="password"
-                :placeholder="$t('Password')"
-                :error-messages="errors"
-                type="password"
-                label="Password"
-                class="mt-3"
-              />
-            </validation-provider>
-            <div class="error-message" v-if="errorMessage">
-              <span>{{ errorMessage }}</span>
-            </div>
-            <v-btn
-              depressed
-              color="primary"
-              id="submit-button"
-              type="submit"
-              :disabled="invalid || serverLoading"
-              class="mr-1 v-btn v-btn--depressed theme--light v-size--default primary"
-            >
-              {{ $t("Log in") }}
-            </v-btn>
-          </div>
-        </form>
-      </validation-observer>
+      <v-container>
+        <v-row>
+          <v-col align="center">
+            <h2>{{ $t("Member login") }}</h2>
+          </v-col>
+          <v-col>
+            <button id="close-button" @click.prevent="dialog = false">
+              &#2573;
+            </button>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-row no-gutters>
+              <v-spacer></v-spacer>
+              <v-col class="col-md-10">
+                {{
+                  $t(
+                    "Log in to post new vacancies or edit existing ones. Don't have an account yet?"
+                  )
+                }}
+              </v-col>
+
+              <v-spacer></v-spacer>
+            </v-row>
+            <v-row no-gutters>
+              <v-spacer></v-spacer>
+              <v-col
+                ><a href="`mailto:${contactEmail}`">{{
+                  $t("Contact us")
+                }}</a></v-col
+              >
+
+              <v-spacer></v-spacer>
+            </v-row>
+          </v-col>
+        </v-row>
+        <validation-observer v-slot="{ invalid }">
+          <form @submit.prevent="login">
+            <v-row no-gutters>
+              <v-spacer></v-spacer>
+              <v-col :align-self="center" class="col-md-7">
+                <validation-provider
+                  name="Username"
+                  rules="required"
+                  v-slot="{ errors }"
+                >
+                  <v-text-field
+                    v-model="username"
+                    :placeholder="$t('Username')"
+                    label="Username"
+                    :error-messages="errors"
+                    autofocus
+                  />
+                </validation-provider>
+              </v-col>
+              <v-spacer></v-spacer>
+            </v-row>
+            <v-row no-gutters>
+              <v-spacer></v-spacer>
+              <v-col :align-self="center" class="col-md-7">
+                <validation-provider
+                  name="Password"
+                  rules="required"
+                  v-slot="{ errors }"
+                >
+                  <v-text-field
+                    v-model="password"
+                    :placeholder="$t('Password')"
+                    :error-messages="errors"
+                    type="password"
+                    label="Password"
+                  />
+                </validation-provider>
+              </v-col>
+              <v-spacer></v-spacer>
+            </v-row>
+            <v-row align="center">
+              <v-col align="center">
+                <div class="error-message" v-if="errorMessage">
+                  <span>{{ errorMessage }}</span>
+                </div>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <v-row no-gutters>
+                  <v-spacer></v-spacer>
+                  <v-col class="col-md-5" :align="center">
+                    <v-btn
+                      depressed
+                      width="100%"
+                      color="primary"
+                      id="submit-button"
+                      type="submit"
+                      :disabled="invalid || serverLoading"
+                      class="mr-1 v-btn v-btn--depressed theme--light v-size--default primary"
+                    >
+                      {{ $t("Log in") }}
+                      <clip-loader :loading="serverLoading"></clip-loader>
+                    </v-btn>
+                  </v-col>
+                  <v-spacer></v-spacer>
+                </v-row>
+              </v-col>
+            </v-row>
+          </form>
+        </validation-observer>
+      </v-container>
     </v-card>
   </v-dialog>
 </template>
@@ -73,6 +123,7 @@
 <script>
 import { mapGetters, mapActions } from "vuex";
 import { ValidationProvider, ValidationObserver } from "vee-validate";
+import { PropagateLoader, ClipLoader } from "@saeris/vue-spinners";
 
 export default {
   name: "AuthState",
@@ -87,7 +138,8 @@ export default {
   },
   components: {
     ValidationProvider,
-    ValidationObserver
+    ValidationObserver,
+    ClipLoader
   },
   computed: {
     ...mapGetters({
@@ -111,6 +163,7 @@ export default {
       });
       this.serverLoading = false;
       if (!this.errorMessage) {
+        this.resetForm();
         this.dialog = false;
         return;
       }
