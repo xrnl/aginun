0ca246fd8afc63a339b620b5a175b731d1093d6b
Tom Ootes <tom@ootes.io>
2021-05-17 21:35:12 +0200

    Feature/authorization hasura 130 (#153)
    * add login modal
    
    * It is highly recommended you commit the generated package lock to source control: this will allow anyone else on your team to get the exact same dependency tree that you were developing on
    
    * Added bearer token to Apollo headers when it is set
    
    * It is highly recommended you commit the generated package lock to source control: this will allow anyone else on your team to get the exact same dependency tree that you were developing on
    
    * Added bearer token to Apollo headers when it is set
    
    * clarified name & functionality of apollo link
    
    * fixed prettier errors
    
    * Added error handling for role card dialogs
    
    * Removed references to API key in front-end code
    
    * Since were not using the API key anymore in the front-end, we can remove this from .env.example
    
    * Change documentation. User still needs to copy the .env.example
    
    * Use the token from the store
    
    * Update SUPPORT.md
    
    Co-authored-by: aleesaan <42735629+aleesaan@users.noreply.github.com>
    
    * Remove unused vars
    
    * Update src/plugins/vue-apollo.ts
    
    Co-authored-by: aleesaan <42735629+aleesaan@users.noreply.github.com>
    
    * Added alerts to the store in stead of in the components
    
    * RoleEditDialog.vue now uses the try/ await structure. Added tests as well
    
    * Clean npm install, committing the new package-lock.json
    
    * resolves #159 by reverting #142
    
    This fix was necessary because passing the language as a custom hasura session
    variable doesn't work with JWT token. See #159 for more context
    
    * removed mention of api key from error page
    
    * clarified support document
    
    * call alerts from roles store and use try/catch
    
    Co-authored-by: Alessandro <alesan.t90@gmail.com>
    Co-authored-by: Tom Ootes <tomootes@gmail.com>
    Co-authored-by: Sam Lubbers <lubbers.sc@gmail.com>
    Co-authored-by: aleesaan <42735629+aleesaan@users.noreply.github.com>

--- src/plugins/vue-apollo.ts
+++ src/plugins/vue-apollo.ts
@@ -2,26 +2,26 @@
 import "unfetch/polyfill";
 import Vue from "vue";
 import VueApollo from "vue-apollo";
-import i18n from "@/i18n/i18n";
 import { ApolloClient } from "apollo-client";
 import { setContext } from "apollo-link-context";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import user from "@/store/modules/user";
 
 Vue.use(VueApollo);
 
 const httpLink = createHttpLink({
-  uri: "http://178.62.229.109/v1/graphql",
-  headers: {
-    "x-hasura-admin-secret": process.env.VUE_APP_API_KEY
-  }
+  uri: "http://178.62.229.109/v1/graphql"
 });
 
-const languageLink = setContext((_, { headers }) => {
+// set request headers based on current application state
+const dynamicLink = setContext((_, { headers }) => {
+  const loginToken = user.state.token;
   return {
     headers: {
       ...headers,
-      "x-hasura-lang": i18n.locale
+      // If the loginToken exists, this property will be added
+      ...(loginToken && { authorization: `Bearer ${loginToken}` })
     }
   };
 });
@@ -29,7 +29,7 @@ const languageLink = setContext((_, { headers }) => {
 const cache = new InMemoryCache();
 
 const apolloClient = new ApolloClient({
-  link: languageLink.concat(httpLink),
+  link: dynamicLink.concat(httpLink),
   cache,
   defaultOptions: {
     query: {
